d08da39094eb4b152855009f68c08dcc454cacfd
Sven Mischkewitz <sven.mkw@gmail.com>
2021-04-25 12:41:43 +0200

    Initial commit and puppeteer demo

---
+++ puppeteer-demo.js
@@ -0,0 +1,27 @@
+const puppeteer = require("puppeteer");
+
+const USER_AGENT =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36";
+
+async function getContent(browser, url) {
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
+  page.setUserAgent(USER_AGENT);
+
+  await page.goto(url, { waitUntil: "networkidle2" });
+  await page.waitForNavigation({ timeout: 0, waitUntil: "networkidle2" });
+  return page.evaluate(() => window.IS24);
+}
+
+async function main() {
+  const browser = await puppeteer.launch({ headless: false });
+
+  const url =
+    "https://www.immobilienscout24.de/Suche/de/berlin/berlin/wohnung-mieten?enteredFrom=one_step_search";
+
+  const content = await getContent(browser, url);
+  console.log(content);
+  await browser.close();
+}
+
+main();
