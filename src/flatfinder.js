84ab647c476f3affd51b94fc7d77df808bab1089
Sven Mischkewitz <sven.mkw@gmail.com>
2021-04-26 12:53:23 +0200

    feat: Send a title message before sending listings

--- src/flatfinder.js
+++ src/flatfinder.js
@@ -13,34 +13,49 @@ const CRAWLERS = {
  * crawlers and sends a Telegram notification for each uniquely new listing.
  */
 async function findFlatsAndNotify() {
-  const notifier = new Notifier(config.telegram);
+  async function findFlats() {
+    let allListings = [];
+    for (let crawlerName in CRAWLERS) {
+      console.log(`Crawling listings for ${crawlerName}.`);
 
-  for (let crawlerName in CRAWLERS) {
-    console.log(`Crawling listings for ${crawlerName}.`);
+      const crawler = CRAWLERS[crawlerName];
+      const urls = config[crawlerName].urls;
+      for (let index in urls) {
+        const url = urls[index];
+        console.log(`${crawlerName} ${parseInt(index) + 1}/${urls.length}`);
+        console.debug(`Crawl URL: ${url}`);
 
-    const crawler = CRAWLERS[crawlerName];
-    const urls = config[crawlerName].urls;
-    for (let index in urls) {
-      const url = urls[index];
-      console.log(`${crawlerName} ${parseInt(index) + 1}/${urls.length}`);
-      console.debug(`Crawl URL: ${url}`);
+        const listings = await crawler(url);
+        console.log(` - Found ${listings.length} listings`);
 
-      const listings = await crawler(url);
-      console.log(` - Found ${listings.length} listings`);
+        const newListings = await store.keepNewListings(listings);
+        console.log(` - Found ${newListings.length} new listings`);
 
-      const newListings = await store.keepNewListings(listings);
-      console.log(` - Found ${newListings.length} new listings`);
-
-      for (let listing of newListings) {
-        console.debug(
-          `  - Add listing ${listing.id}: ${listing.title} | ${listing.url}`
-        );
-        await notifier.sendMessage(listing);
+        allListings = allListings.concat(newListings);
       }
-
-      await store.insertListing(newListings);
     }
+    return allListings;
   }
+
+  async function notify(listings) {
+    if (listings.length === 0) {
+      return;
+    }
+
+    const notifier = new Notifier(config.telegram);
+    await notifier.sendUpdateTitle(listings);
+    for (let listing of listings) {
+      console.debug(
+        `  - Add listing ${listing.id}: ${listing.title} | ${listing.url}`
+      );
+      await notifier.sendListing(listing);
+    }
+
+    await store.insertListing(listings);
+  }
+
+  const listings = await findFlats();
+  return notify(listings);
 }
 
 module.exports = findFlatsAndNotify;
--- src/notifier.js
+++ src/notifier.js
@@ -8,7 +8,17 @@ class Notifier {
     this.bot = new TelegramBot(this.token);
   }
 
-  async sendMessage(listing) {
+  async sendUpdateTitle(listings) {
+    let message;
+    if (listings.length > 1) {
+      message = `*Found ${listings.length} new listing, yay* ` + "\u{1F389}";
+    } else {
+      message = "*Found a new listing, yay* \u{1F389}";
+    }
+    return this.bot.sendMessage(this.chatId, message);
+  }
+
+  async sendListing(listing) {
     const title = `<a href="${listing.url}"><b>${listing.title}</b></a>\n`;
     const rooms = `Rooms: <code>${listing.rooms}</code>\n`;
     const size = `Size: <code>${listing.size}m²</code>\n`;
